8f6f260936119b7125d4ca7ddf6318d6ea4a1116
dima <nevar.dima@gmail.com>
2023-05-18 13:44:29 +0300

    Frontend part of ai image generator

--- start.js
+++ start.js
@@ -1,3 +1,5 @@
+
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 8080;
@@ -8,6 +10,8 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/openai', openai);
 
